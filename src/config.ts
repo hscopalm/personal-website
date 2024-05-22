3d44864b51a76497b22cc7b7e23c81ab70e60176
Harrison Palmer <hscopalm@gmail.com>
2024-05-21 23:33:50 -0700

    Initial config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Harrison Palmer";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Welcome to my page! I'm a data engineer and adventure blogger. I write about my travels, data projects, and other random thoughts.";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+export const MY_NAME = "Harrison Palmer";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
